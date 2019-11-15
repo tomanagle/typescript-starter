8124f919c3b3f02e686f2a58ffba0f32929c8cb2
Tom Nagle <tomanagle@gmail.com>
2019-11-15 17:02:32 +1100

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-export const world = '🗺️';
+import http from 'http'
+import './db/connect'
+import User from './model/user.model'
 
-export function hello(word: string = world): string {
-  return `Hello ${world}`;
-}
+//create a server object:
+http.createServer(async function(req, res) {
+    res.write('Hello World!') //write a response
+    res.end() //end the response
 
-console.log(hello());
+    const users = await User.find().exec()
+}).listen(3000, function() {
+    console.log('server start at port 3000') //the server object listens on port 3000
+})
